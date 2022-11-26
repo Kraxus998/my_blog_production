e2cad5ea9a9ce4df683e87953c44a128741e50ae
Kraxus998 <81646990+Kraxus998@users.noreply.github.com>
2022-11-26 15:19:18 -0400

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-y&w_1$$czqvrq)_k3*)8hoiaz6qi!&fkcn#+4=dbsuk0*_sxs7'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-y&w_1$$czqvrq)_k3*)8hoiaz6qi!&fkcn#+4=dbsuk0*_sxs7')
-CSRF_TRUSTED_ORIGINS = ['web-production-22ce.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-22ce.up.railway.app']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
